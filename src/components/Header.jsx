990844b2891390314e0a4c59d30c3d8afcafb4ab
JamieJay <jwjh1205@naver.com>
2023-11-15 18:43:38 +0900

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+function Header() {
+  return (
+    <header className="flex flex-col items-center my-8 md:mb-16">
+      <h1 className="text-xl md:text-4xl font-semibold tracking-widest text-center uppercase text-white font-title">
+        예산 계산기
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
